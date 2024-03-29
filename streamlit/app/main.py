06696e067607bc290fe0122b4e2f696d7ed17360
João Pedro <joaopedrodasilvalima@gmail.com>
2024-03-29 20:04:09 -0300

    FEAT: Parâmetros na URL e remover instalação do psycopg2

--- streamlit/app/main.py
+++ streamlit/app/main.py
@@ -4,5 +4,32 @@ import re
 
 from maps import generate_brazil_map_with_ufs_and_links
 
-st.title('Hello World!')
-st.markdown(generate_brazil_map_with_ufs_and_links(), unsafe_allow_html=True)
+UFS = [
+    "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS",
+    "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC",
+    "SP", "SE", "TO", "ZZ", "ALL"
+]
+TURNOS = ['1', '2']
+
+def get_parameters():
+    query_parameters = st.query_params
+    select_parameters = lambda x, default, accepted: (
+        default 
+        if x not in query_parameters 
+        else query_parameters[x] if query_parameters[x] in accepted
+        else default
+    )
+
+    uf = select_parameters('uf', 'ALL', UFS)
+    turno = select_parameters('turno', 1, TURNOS)
+    
+    return uf, turno
+
+# st.markdown(generate_brazil_map_with_ufs_and_links(), unsafe_allow_html=True)
+if __name__ == "__main__":
+    st.set_page_config(layout="wide")
+
+    uf, turno = get_parameters()
+
+    st.title(f'{turno}º Turno {uf} - Tempo de Votação')
+    st.divider()
